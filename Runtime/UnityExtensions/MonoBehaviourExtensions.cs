89444072682a37ef8228812353e5355f79d49ecd
Akhil Gupta <akhil.gupta.7295@gmail.com>
2025-08-23 23:17:50 +0530

    added MonoBehaviourExtensions.cs

---
+++ Runtime/UnityExtensions/MonoBehaviourExtensions.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Aklgupta.Utils.UnityExtensions {
+	public static class MonoBehaviourExtensions {
+
+		[Conditional("UNITY_EDITOR")]
+		public static void Log(this object source, object obj) {
+			Debug.Log($"Ext [{source.GetType().Name}] : {obj}", source as Object);
+		}
+		
+	}
+}
