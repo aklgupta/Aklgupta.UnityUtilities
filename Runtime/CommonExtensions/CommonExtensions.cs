ce1f42d410d575dd37f7cf517b9a4881578d3181
Akhil Gupta <akhil.gupta.7295@gmail.com>
2025-08-27 21:38:08 +0530

    improve list random

--- Runtime/CommonExtensions/CommonExtensions.cs
+++ Runtime/CommonExtensions/CommonExtensions.cs
@@ -7,6 +7,16 @@ namespace Aklgupta.Utils.CommonExtensions {
 		private static readonly Random random = new();
 
 		public static T Random<T>(this IList<T> list) {
+			if (list == null)
+				throw new ArgumentNullException(nameof(list), "Can't get random element from null");
+
+			if (list.Count == 0)
+				throw new ArgumentException("List has 0 elements. Can't return a random value");
+
+			return list[random.Next(list.Count)];
+		}
+
+		public static T RandomOrDefault<T>(this IList<T> list) {
 			if (list == null || list.Count == 0)
 				return default;
 
